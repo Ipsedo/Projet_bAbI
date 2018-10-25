609519c7854c0afadf398fcbf7b16948decba430
samuel <berriens@gmail.com>
2018-10-25 09:10:53 +0200

    fait boucle apprentissage

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
 #!/usr/bin/env python
 import model.fst_model as mod
 import torch as th
+
 import data.data_processing as data_process
 
 
-# test donnée
-f= open("./res/tasks_1-20_v1-2/en/qa1_single-supporting-fact_train.txt","r")
+# Données de train
+f = open("./res/tasks_1-20_v1-2/en/qa1_single-supporting-fact_train.txt","r")
 data_train = data_process.process_data(data_process.split_file(f))
-data=data_process.split_sentence(data_train)
-vocab, prepared_data = data_process.make_vocab_and_transform_data(data)
+data_train = data_process.split_sentence(data_train)
+vocab_train, data_train = data_process.make_vocab_and_transform_data(data_train)
+f.close()
 
-taille_vocab=len(vocab)
-taille_embedding = 9
-taille_hidden_story = 30
-taille_hidden_quest = 60
+# Données de test
+f = open("./res/tasks_1-20_v1-2/en/qa1_single-supporting-fact_test.txt","r")
+data_test = data_process.process_data(data_process.split_file(f))
+data_test = data_process.split_sentence(data_test)
+data_test = data_process.make_data_with_vocab(data_test, vocab_train)
+f.close()
+
+taille_vocab=len(vocab_train)
+taille_embedding = 10
+taille_hidden_story = 5
+taille_hidden_quest = 7
 
 model = mod.MyModel(taille_vocab, taille_embedding, taille_hidden_story, taille_hidden_quest)
-model.train()
-out=model(prepared_data[0])
-print(out)
-print(len(vocab))
+optimizer = th.optim.Adagrad(model.parameters(), lr=1e-2)
+loss_fn = th.nn.CrossEntropyLoss()
 
-s1=prepared_data[50][0].numpy()
+nbEpoch = 20
+
+for e in range(nbEpoch):
+	model.train()
+	nb_train = 0
+	epoch_loss = 0
+	for chall in data_train:
+		model.zero_grad()
+		out = model(chall)
+		loss = loss_fn(out.unsqueeze(0), th.LongTensor([chall[2]]))
+		loss.backward()
+		optimizer.step()
+		nb_train += 1
+		epoch_loss += loss.item()
+
+	nb_test = 0
+	nb_err = 0
+	model.eval()
+	for chall in data_test:
+		out = model(chall)
+		nb_err += 1 if out.argmax(0) != chall[2] else 0
+		nb_test += 1
+
+	print("Epoch (%d) : err_rate = %f, loss = %f" % (e, nb_err / nb_test, epoch_loss / nb_train))
+
+
+model.train()
+out=model(data_train[0])
+print(out)
+print(len(vocab_train))
+
+s1=data_train[50][0].numpy()
 print(type(s1))
 str=""
-inv_map = {v: k for k, v in vocab.items()}
+inv_map = {v: k for k, v in vocab_train.items()}
 for d in s1:
 	str += inv_map[d]
 print(str)
