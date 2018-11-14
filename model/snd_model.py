b00fd30c0babb08e618f2256e8f6769d79437d31
samuel <berriens@gmail.com>
2018-11-14 22:39:07 +0100

    test différente convolution

--- model/snd_model.py
+++ model/snd_model.py
@@ -1,25 +1,34 @@
 import torch as th
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class SndModel(nn.Module):
 
-	def __init__(self, vocab_size, embedding_size, story_hidden_size, quest_hidden_size):
+	def __init__(self, vocab_size, embedding_size, story_hidden_size, quest_hidden_size, story_max_len, quest_max_len):
 		super(SndModel, self).__init__()
 
 		self.vocab_size = vocab_size
 		self.embedding_size = embedding_size
 		self.story_hidden_size = story_hidden_size
 		self.quest_hidden_size = quest_hidden_size
+		self.story_max_len = story_max_len
+		self.quest_max_len = quest_max_len
+		self.out_channel_conv = 20
 
 		self.embedding_story = nn.Embedding(self.vocab_size, self.embedding_size)
 		self.rnn_story_1 = nn.RNN(self.embedding_size, self.story_hidden_size)
-		self.rnn_story_2 = nn.RNN(self.embedding_size, self.story_hidden_size)
 
 		self.embedding_quest = nn.Embedding(self.vocab_size, self.embedding_size)
 		self.rnn_quest = nn.RNN(self.embedding_size, self.quest_hidden_size)
 
-		self.lin = nn.Linear(2*self.story_hidden_size + self.quest_hidden_size, self.vocab_size)
+		kernel_sizes = [3, 4, 5]
+		self.conv_story = nn.ModuleList(
+			[nn.Conv2d(1, self.out_channel_conv, (k, self.story_hidden_size)) for k in kernel_sizes])
+		self.maxpool_story = nn.ModuleList(
+			[nn.MaxPool1d(self.story_max_len + self.quest_max_len - k + 1, self.out_channel_conv) for k in kernel_sizes])
+
+		self.lin = nn.Linear(self.out_channel_conv*len(kernel_sizes), self.vocab_size)
 		self.act = nn.LogSoftmax(dim=0)
 
 	def forward(self, inp):
@@ -27,24 +36,26 @@ class SndModel(nn.Module):
 		# story.shape=(nb_mot_story)
 		# quest.shape=(nb_mot_quest)
 
-		outStory = self.embedding_story(story)
+		out_story = self.embedding_story(story)
 		# outStory.shape = (nb_mot_story, taille_embedding)
-		outStory1, _ = self.rnn_story_1(outStory.unsqueeze(1), th.zeros(1, 1, self.story_hidden_size))
-		outStory2, _ = self.rnn_story_2(reversed(outStory).unsqueeze(1), th.zeros(1, 1, self.story_hidden_size))
+		out_story, _ = self.rnn_story_1(out_story.unsqueeze(1), th.zeros(1, 1, self.story_hidden_size))
 		# outStory.shape = (nb_mot, 1, story_hidden_size)
 
-		outQuest = self.embedding_quest(quest)
-		outQuest, _ = self.rnn_quest(outQuest.unsqueeze(1), th.zeros(1, 1, self.quest_hidden_size))
+		out_quest = self.embedding_quest(quest)
+		out_quest, _ = self.rnn_quest(out_quest.unsqueeze(1), th.zeros(1, 1, self.quest_hidden_size))
 
-		outStory1 = outStory1.squeeze(1) # outStory.shape = (nb_mot, story_hidden_size)
-		outStory2 = outStory2.squeeze(1)
-		outQuest = outQuest.squeeze(1)
+		out_story = out_story.squeeze(1) # outStory.shape = (nb_mot, story_hidden_size)
+		out_quest = out_quest.squeeze(1)
 
-		outStory1 = outStory1[-1]  # get last element
-		outStory2 = outStory2[0]
-		outQuest = outQuest[-1]  # get last element
+		out_story = F.pad(out_story, (0, 0, 0, self.story_max_len - out_story.size(0)), 'constant', 0)
+		out_quest = F.pad(out_quest, (0, 0, 0, self.quest_max_len - out_quest.size(0)), 'constant', 0)
 
-		out = th.cat((outStory1, outStory2, outQuest))
+		outs = th.cat((out_story, out_quest)).unsqueeze(0).unsqueeze(0)
+
+		outs = [conv2d(outs).squeeze(3) for conv2d in self.conv_story]
+		outs = [pool(out).squeeze(0).squeeze(1) for pool, out in zip(self.maxpool_story, outs)]
+
+		out = th.cat(outs)
 		out = self.lin(out)
 		return self.act(out)
 
